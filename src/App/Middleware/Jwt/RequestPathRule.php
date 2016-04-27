ebfef417482f58a981ca97407e2b7ed905b52d41
Arber Mustafa <arber9@gmail.com>
2016-04-27 14:25:59 +0200

    Create RequestPathRule.php

---
+++ src/App/Middleware/Jwt/RequestPathRule.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Middleware\Jwt;
+
+class RequestPathRule implements \Slim\Middleware\JwtAuthentication\RuleInterface
+{
+    protected $options = array(
+        "path" => array("/"),
+        "passthrough" => array()
+    );
+
+    public function __construct($options = array())
+    {
+        $this->options = array_merge($this->options, $options);
+    }
+
+    public function __invoke(\Slim\Slim $app)
+    {
+        $uri = $app->request->getRootUri() . $app->request->getResourceUri();
+
+        $uri = str_replace("//", "/", $uri);
+
+        /* If request path is matches passthrough should not authenticate. */
+        foreach ((array)$this->options["passthrough"] as $passthrough) {
+            $passthrough = rtrim($passthrough, "/");
+            if (!!preg_match("@^{$passthrough}(/.*)?$@", $uri)) {
+                return false;
+            }
+        }
+
+        /* Otherwise check if path matches and we should authenticate. */
+        foreach ((array)$this->options["path"] as $path) {
+            $path = rtrim($path, "/");
+            if (!!preg_match("@^{$path}(/.*)?$@", $uri)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
