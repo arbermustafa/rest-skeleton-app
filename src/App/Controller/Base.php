defd790048e1592dc451bea72a0b7e4455bea41d
Arber Mustafa <arber9@gmail.com>
2016-04-27 14:21:09 +0200

    Update Base.php

--- src/App/Controller/Base.php
+++ src/App/Controller/Base.php
@@ -6,15 +6,42 @@ class Base
     /**
      * Slim instance
      */
-    private static function _getApp()
+    protected static function _getApp()
     {
         return \Slim\Slim::getInstance();
+    }
+
+    protected static function getParams()
+    {
+        $request = self::_getApp()->request;
+
+        if ($request->getMediaType() === 'application/json') {
+            return self::buildArrayRequest($request->getBody());
+        } else {
+            return $request->params();
+        }
+    }
+
+    /**
+     * Build array request from body payload
+     */
+    protected static function buildArrayRequest($request)
+    {
+        if (!empty($request)) {
+            $json = json_decode($request, true);
+
+            if (!empty($json)) {
+                return $json;
+            }
+        }
+
+        return array();
     }
 
     /**
      * Render JSON Response
      */
-    public static function response($result, $status = 200, $error = false)
+    protected static function response($result, $status = 200, $error = false)
     {
         self::_getApp()
             ->render($status, array(
@@ -26,7 +53,7 @@ class Base
     /**
      * Render default JSON Response
      */
-    public static function welcome()
+    protected static function welcome()
     {
         self::response('There is a RESTful webservice in here :)');
     }
